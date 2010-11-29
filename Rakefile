bde3898a6c4a280e66c3970cca0d194106d674eb
Rene Saarsoo <nene@triin.net>
2010-11-29 20:46:07 +0200

    Replaced class unit test with rspec test.
    Eventually I would like to move all unit tests to rspec.

--- Rakefile
+++ Rakefile
@@ -1,11 +1,19 @@
 require 'rubygems'
 require 'rake'
 
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
+
 require 'rake/testtask'
 Rake::TestTask.new(:test) do |test|
-  test.libs << 'lib' << 'test'
   test.pattern = 'test/**/tc_*.rb'
   test.verbose = true
+end
+
+require 'spec'
+require 'spec/rake/spectask'
+Spec::Rake::SpecTask.new(:spec) do |spec|
+  spec.spec_opts = ["--color"]
+  spec.spec_files = FileList["spec/**/*_spec.rb"]
 end
 
 task :default => :test
---
+++ spec/class_spec.rb
@@ -0,0 +1,85 @@
+require "jsduck/class"
+
+describe Class do
+
+  describe "#members_hash" do
+
+    before do
+      @classes = {}
+      @parent = JsDuck::Class.new({
+          :name => "ParentClass",
+          :method => [
+            {:name => "baz"},
+            {:name => "foo"},
+            {:name => "frank", :private => true},
+          ]
+        }, @classes);
+      @classes["ParentClass"] = @parent
+      @child = JsDuck::Class.new({
+          :name => "ChildClass",
+          :extends => "ParentClass",
+          :method => [
+            {:name => "foo"},
+            {:name => "bar"},
+            {:name => "zappa", :private => true},
+          ]
+        }, @classes);
+      @classes["ChildClass"] = @child
+    end
+
+    it "returns all public members in current class" do
+      ms = @parent.members_hash(:method)
+      ms.values.length.should == 2
+      ms["foo"][:member].should == "ParentClass"
+      ms["baz"][:member].should == "ParentClass"
+    end
+
+    it "also returns all public members in parent class" do
+      ms = @child.members_hash(:method)
+      ms.values.length.should == 3
+      ms["foo"][:member].should == "ChildClass"
+      ms["bar"][:member].should == "ChildClass"
+      ms["baz"][:member].should == "ParentClass"
+    end
+  end
+
+  describe "#inherits_from" do
+
+    before do
+      @classes = {}
+      @parent = JsDuck::Class.new({
+        :name => "Parent",
+      }, @classes);
+      @classes["Parent"] = @parent
+
+      @child = JsDuck::Class.new({
+        :name => "Child",
+        :extends => "Parent",
+      }, @classes);
+      @classes["Child"] = @child
+
+      @grandchild = JsDuck::Class.new({
+        :name => "GrandChild",
+        :extends => "Child",
+      }, @classes);
+      @classes["GrandChild"] = @grandchild
+    end
+
+    it "true when asked about itself" do
+      @parent.inherits_from?("Parent").should == true
+    end
+
+    it "false when asked about class it's not inheriting from" do
+      @parent.inherits_from?("Child").should == false
+    end
+
+    it "true when asked about direct parent" do
+      @child.inherits_from?("Parent").should == true
+    end
+
+    it "true when asked about grandparent" do
+      @grandchild.inherits_from?("Parent").should == true
+    end
+  end
+
+end
