bca4add1f53f42de897ec1922f55c0784e5b597d
Rene Saarsoo <nene@triin.net>
2010-12-05 22:24:17 +0200

    Rendering return doc for methods.

--- lib/jsduck/method_table.rb
+++ lib/jsduck/method_table.rb
@@ -16,17 +16,33 @@ module JsDuck
       @short_params.render(item[:params]) + " : " + return_type(item)
     end
 
-    def return_type(item)
-      item[:return] ? (item[:return][:type] || "void") : "void"
-    end
-
     def extra_doc(item)
       [
         "<div class='mdetail-params'>",
         "<strong>Parameters:</strong>",
         @long_params.render(item[:params]),
+        "<strong>Returns:</strong>",
+        render_return(item),
         "</div>"
       ].join("\n")
+    end
+
+    def render_return(item)
+      type = return_type(item)
+      doc = return_doc(item)
+      if type == "void" && doc.length == 0
+        "<ul><li>void</li></ul>"
+      else
+        "<ul><li><code>#{type}</code><div class='sub-desc'>#{doc}</div></li></ul>"
+      end
+    end
+
+    def return_type(item)
+      item[:return] ? (item[:return][:type] || "void") : "void"
+    end
+
+    def return_doc(item)
+      item[:return] ? (item[:return][:doc] || "") : ""
     end
   end
 
