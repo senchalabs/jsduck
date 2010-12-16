3bbcc3dca51e06ce4e198f5d1535f5566c5b68ca
Rene Saarsoo <nene@triin.net>
2010-12-16 23:31:39 +0200

    Helper method for dealing with heredoc strings.

--- spec/doc_formatter_spec.rb
+++ spec/doc_formatter_spec.rb
@@ -1,5 +1,28 @@
 require "jsduck/doc_formatter"
 
+class String
+  # Removes beginning-whitespace from each line of a string.
+  # But only as many whitespace as the first line has.
+  #
+  # Ment to be used with heredoc strings like so:
+  #
+  # text = <<-EOS.heredoc
+  #   This line has no indentation
+  #     This line has 2 spaces of indentation
+  #   This line is also not indented
+  # EOS
+  #
+  def unindent
+    lines = []
+    each_line {|ln| lines << ln }
+
+    first_line_ws = lines[0].match(/^\s+/)[0]
+    re = Regexp.new('^\s{0,' + first_line_ws.length.to_s + '}')
+
+    lines.collect {|line| line.sub(re, "") }.join
+  end
+end
+
 describe JsDuck::DocFormatter do
 
   before do
@@ -62,12 +85,12 @@ describe JsDuck::DocFormatter do
 
     describe "<pre>" do
       before do
-        @html = @formatter.format(<<-EOS)
-Some code<pre>
-if (condition) {
-    doSomething();
-}
-</pre>
+        @html = @formatter.format(<<-EOS.unindent)
+          Some code<pre>
+          if (condition) {
+              doSomething();
+          }
+          </pre>
         EOS
       end
 
@@ -80,12 +103,12 @@ if (condition) {
 
     describe "<pre><code>" do
       before do
-        @html = @formatter.format(<<-EOS)
-Some code<pre><code>
-if (condition) {
-    doSomething();
-}
-</code></pre>
+        @html = @formatter.format(<<-EOS.unindent)
+          Some code<pre><code>
+          if (condition) {
+              doSomething();
+          }
+          </code></pre>
         EOS
       end
 
