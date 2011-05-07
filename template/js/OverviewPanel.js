158f7e355553915eca16645223b5a30efb487dbf
Rene Saarsoo <nene@triin.net>
2011-05-07 22:53:12 +0300

    Initial templates for members.

--- template/js/OverviewPanel.js
+++ template/js/OverviewPanel.js
@@ -57,6 +57,70 @@ Ext.define('Docs.OverviewPanel', {
         }
 
         this.callParent(arguments);
+
+        var cfgTemplate = new Ext.XTemplate(
+            '<div class="member f ni">',
+                '<a href="Ext.Action.html#config-disabled" rel="config-disabled" class="expand more">',
+                    '<span>&nbsp;</span>',
+                '</a>',
+                '<div class="title">',
+                    '<div class="meta">',
+                        '<a href="Ext.Action.html" class="definedIn docClass">{member}</a><br/>',
+                        '<a href="../source/Action.html#Ext-Action-cfg-disabled" class="viewSource">view source</a>',
+                    '</div>',
+                    '<a name="disabled"></a>',
+                    '<a name="config-disabled"></a>',
+                    '<a href="Ext.Action.html#" rel="config-disabled" class="cls expand">{name}</a>',
+                    '<span> : {type}</span>',
+                '</div>',
+                '<div class="description">',
+                    '{doc}',
+                '</div>',
+            '</div>'
+        );
+
+        this.tpl = new Ext.XTemplate(
+            '{doc}',
+            '<div class="members">',
+                '<div class="m-cfgs">',
+                    '<div class="definedBy">Defined By</div>',
+                    '<a name="configs"></a>',
+                    '<h3 class="cfg p">Config Options</h3>',
+                    '<tpl for="cfg">',
+                        '{[this.renderMember(values)]}',
+                    '</tpl>',
+                '</div>',
+                '<div class="m-properties">',
+                    '<div class="definedBy">Defined By</div>',
+                    '<a name="properties"></a>',
+                    '<h3 class="prp p">Properties</h3>',
+                    '<tpl for="property">',
+                        '{[this.renderMember(values)]}',
+                    '</tpl>',
+                '</div>',
+                '<div class="m-methods">',
+                    '<div class="definedBy">Defined By</div>',
+                    '<a name="methods"></a>',
+                    '<h3 class="mth p">Methods</h3>',
+                    '<tpl for="method">',
+                        '{[this.renderMember(values)]}',
+                    '</tpl>',
+                '</div>',
+                '<div class="m-events">',
+                    '<div class="definedBy">Defined By</div>',
+                    '<a name="events"></a>',
+                    '<h3 class="evt p">Events</h3>',
+                    '<tpl for="event">',
+                        '{[this.renderMember(values)]}',
+                    '</tpl>',
+                '</div>',
+            '</div>',
+            {
+                renderMember: function(cfg) {
+                    return cfgTemplate.apply(cfg);
+                }
+            }
+        );
     },
 
     load: function(docClass) {
@@ -66,6 +130,6 @@ Ext.define('Docs.OverviewPanel', {
       });
       this.addDocked(this.toolbar);
 
-      this.update(docClass.doc);
+      this.update(this.tpl.apply(docClass));
     }
 });
