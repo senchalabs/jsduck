693af00df04b7d898f3ff0117313c7cbbee9aac4
Rene Saarsoo <nene@triin.net>
2011-05-13 12:01:06 +0300

    Removed Array.prototype.forEach - used no more.
    Instead we are using Ext.Array.forEach.

--- template/js/functions.js
+++ template/js/functions.js
@@ -1,21 +1,5 @@
 // Bunch of global functions and variables
 
-if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function(fun /*, thisp*/) {
-        var len = this.length;
-        if (typeof fun != "function") {
-            throw new TypeError();
-        }
-
-        var thisp = arguments[1];
-        for (var i = 0; i < len; i++) {
-            if (i in this) {
-                fun.call(thisp, this[i], i, this);
-            }
-        }
-    };
-}
-
 var classCache = {};
 var getDocClass = function(cls, noHistory) {
     var member,
