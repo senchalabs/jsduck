34095ec69bd4252c2bdca3513bc3daafa041ed1a
Rene Saarsoo <nene@triin.net>
2011-08-02 20:42:48 +0300

    Rename "Find class members" to "Filter class members".

--- template/app/view/cls/Toolbar.js
+++ template/app/view/cls/Toolbar.js
@@ -74,7 +74,7 @@ Ext.define('Docs.view.cls.Toolbar', {
         this.items = this.items.concat([
             { xtype: 'tbspacer', width: 10 },
             this.filterField = Ext.widget("textfield", {
-                emptyText: 'Find class members...',
+                emptyText: 'Filter class members',
                 enableKeyEvents: true,
                 listeners: {
                     keyup: function(cmp) {
