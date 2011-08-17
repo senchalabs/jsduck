f99ef124b8bda69aa44e1d67dcfc29f59b49d86b
Rene Saarsoo <nene@triin.net>
2011-08-17 11:44:14 -0700

    Add ugly buttons for switching class tree logic.

--- template/app/view/cls/Tree.js
+++ template/app/view/cls/Tree.js
@@ -15,8 +15,51 @@ Ext.define('Docs.view.cls.Tree', {
      */
 
     initComponent: function() {
-        var tree = new Docs.view.cls.InheritanceLogic({classes: this.data, showPrivateClasses: true});
-        this.root = tree.create();
+        this.setLogic(Docs.view.cls.PackageLogic);
+
+        this.buttons = [
+            {
+                xtype: 'button',
+                text: 'By package',
+                toggleGroup: 'logic',
+                pressed: true,
+                allowDepress: false,
+                handler: function() {
+                    this.setLogic(Docs.view.cls.PackageLogic);
+                },
+                scope: this
+            },
+            {
+                xtype: 'button',
+                text: 'By inheritance',
+                toggleGroup: 'logic',
+                allowDepress: false,
+                handler: function() {
+                    this.setLogic(Docs.view.cls.InheritanceLogic);
+                },
+                scope: this
+            }
+        ];
+
         this.callParent();
+    },
+
+    setLogic: function(logic) {
+        var tree = new logic({classes: this.data, showPrivateClasses: true});
+        if (this.root) {
+            // remember the current selection
+            var selected = this.getSelectionModel().getLastSelected();
+            // create new treestructure
+            var root = tree.create();
+            this.setRootNode(root);
+            this.initNodeLinks();
+            // expand first child
+            this.getRootNode().getChildAt(0).expand();
+            // re-establish the previous selection
+            selected && this.selectUrl(selected.raw.url);
+        }
+        else {
+            this.root = tree.create();
+        }
     }
 });
