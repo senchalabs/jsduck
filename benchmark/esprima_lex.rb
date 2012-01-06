4cf671e1ad6499a2fe5758258d9f7c4468d2b5de
Rene Saarsoo <nene@triin.net>
2012-01-06 15:30:59 +0200

    Comparison of old and Esprima lexer.
    Unfortunately Esprima is way slower :(

--- benchmark/esprima_lex.rb
+++ benchmark/esprima_lex.rb
@@ -3,10 +3,10 @@
 # Takes bunch of filenames as arguments and runs them all through lexer.
 #
 $:.unshift File.dirname(File.dirname(__FILE__)) + "/lib"
-require 'jsduck/lexer'
+require 'jsduck/esprima_lexer'
 
 ARGV.each do |fname|
-  lex = JsDuck::Lexer.new(IO.read(fname))
+  lex = JsDuck::EsprimaLexer.new(IO.read(fname))
   while !lex.empty?
     lex.next
   end
