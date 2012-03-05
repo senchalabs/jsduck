cf71bff8a77c0d02a1f035f3f23f9eee18d3f600
Rene Saarsoo <nene@triin.net>
2012-03-05 17:36:11 +0200

    Remove off-topic comment from JsDuck::Tag::Aside.

--- lib/jsduck/tag/aside.rb
+++ lib/jsduck/tag/aside.rb
@@ -3,9 +3,6 @@ require "jsduck/logger"
 
 module JsDuck::Tag
   # Implementation of @aside tag.
-  #
-  # To document members that were present in previous version but are
-  # completely gone now.  Other than that it behaves exactly like @deprecated.
   class Aside < JsDuck::MetaTag
     def initialize
       @name = "aside"
