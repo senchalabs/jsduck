8164ac03062813cc544a30e32f8b0d071191ec9b
Rene Saarsoo <nene@triin.net>
2012-04-26 12:38:39 +0300

    Update 404 error message to suggest connection problems.
    Because we're using JsonP requests the browser simply doesn't give us
    any information as of why the loading of <script> tag failed - it just
    says: "error".

--- template/app/controller/Failure.js
+++ template/app/controller/Failure.js
@@ -25,8 +25,8 @@ Ext.define('Docs.controller.Failure', {
         var tpl = new Ext.XTemplate(
             "<h1>Oops...</h1>",
             "<p>{msg}</p>",
-            "<p>Maybe it was renamed to something else? Or maybe it has passed away permanently to the 404 land? ",
-            "This would be sad. Hopefully it's just a bug in our side. ",
+            "<p>Maybe it was renamed to something else? Or maybe your internet connection has failed? ",
+            "This would be sad. Hopefully it's just a bug on our side. ",
             "Report it to <a href='http://www.sencha.com/forum/showthread.php?135036'>Sencha Forum</a> if you feel so.</p>",
             "<p>Sorry for all this :(</p>"
         );
