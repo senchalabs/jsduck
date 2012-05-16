f31064aef175cf3d4b14af9a335acb7c4bf6fbb8
Robert Dougan <rdougan@me.com>
2012-05-16 14:54:47 -0700

    Implement hiding by current user.
    Masking recent comments while loading, and remembering of filters
    of recent comments.

--- template/app/view/comments/Index.js
+++ template/app/view/comments/Index.js
@@ -6,26 +6,69 @@ Ext.define('Docs.view.comments.Index', {
     alias: 'widget.commentindex',
     mixins: ['Docs.view.Scrolling'],
 
-    cls: 'comment-index iScroll',
+    cls: 'comment-index',
     margin: '10 0 0 0',
-    autoScroll: true,
+    layout: 'fit',
 
-    items: [
+    dockedItems: [
         {
-            xtype: 'container',
+            xtype: 'component',
+            dock: 'top',
             html: [
-                '<h1>Recent Comments</h1>',
-                '<ul id="comment-index-controls">',
-                    '<li><label><input type="checkbox" name="hideRead" id="hideRead" /> Hide read</label></li>',
-                    '<li><label><input type="checkbox" name="sortByScore" id="sortByScore" /> Sort by score</label></li>',
-                '</ul>'
+                '<h1>Recent Comments</h1>'
             ].join(" ")
         },
         {
             xtype: 'container',
-            id: 'recentcomments'
+            dock: 'left',
+            width: 200,
+            html: [
+                '<ul id="comment-index-controls">',
+                    '<li><label><input type="checkbox" name="hideRead" id="hideRead" /> Hide read</label></li>',
+                    '<li><label><input type="checkbox" name="hideCurrentUser" id="hideCurrentUser" /> Hide current User</label></li>',
+                    '<li><label><input type="checkbox" name="sortByScore" id="sortByScore" /> Sort by score</label></li>',
+                '</ul>'
+            ].join(" ")
         }
     ],
+
+    items: [
+        {
+            cls: 'iScroll',
+            id: 'recentcomments-container',
+            autoScroll: true,
+            items: [
+                {
+                    xtype: 'container',
+                    id: 'recentcomments'
+                }
+            ]
+        }
+    ],
+
+    afterRender: function() {
+        this.callParent(arguments);
+        this.initCookies();
+
+        Ext.get('recentcomments-container').mask();
+    },
+
+    /**
+     * Loops through each of the filter values (manually set for now) and checked for a previous set cookie value.
+     */
+    initCookies: function() {
+        var checkboxes = ['hideRead', 'hideCurrentUser', 'sortByScore'],
+            ln = checkboxes.length,
+            i, el;
+
+        for (i = 0; i < ln; i++) {
+            name = checkboxes[i];
+            el = Ext.get(name);
+            if (el) {
+                el.dom.checked = Boolean(Ext.util.Cookies.get(name));
+            }
+        }
+    },
 
     /**
      * Returns tab config for comments page.
