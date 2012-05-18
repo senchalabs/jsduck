5983d95eaea442db0ae38806e29f53eb21b6ac60
Rene Saarsoo <nene@triin.net>
2012-05-18 16:28:31 +0300

    Remove old esprima_parse.rb benchmark.
    The parse.rb benchmark now uses Esprima directly.
    
    Also fix the serializer benchmark (EsprimaCore was renamed to Esprima).

--- benchmark/serialize.rb
+++ benchmark/serialize.rb
@@ -4,11 +4,11 @@
 # parser and serializes the resulting syntax trees.
 #
 $:.unshift File.dirname(File.dirname(__FILE__)) + "/lib"
-require 'jsduck/esprima_core'
+require 'jsduck/esprima'
 require 'jsduck/serializer'
 
 ARGV.each do |fname|
-  ast = JsDuck::EsprimaCore.instance.parse(IO.read(fname))
+  ast = JsDuck::Esprima.instance.parse(IO.read(fname))
   JsDuck::Serializer.new.to_s(ast)
 end
 
