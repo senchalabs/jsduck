29f42934f14ab747d341bebc8c3d3da084032d1d
Rene Saarsoo <nene@triin.net>
2012-08-24 16:00:33 +0300

    Shorten fields in targets table.
    MySQL has restriction for index size, so make the fields shorter.
    100 should be enough for class and member names.
    50 should be enough for domain name.

--- comments/schema.sql
+++ comments/schema.sql
@@ -8,10 +8,10 @@ CREATE TABLE users (
 
 CREATE TABLE targets (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    domain VARCHAR(255) NOT NULL, -- combines sdk & version
+    domain VARCHAR(50) NOT NULL, -- combines sdk & version
     type ENUM('class', 'guide', 'video', 'unknown', 'challenge') NOT NULL DEFAULT 'class',
-    cls VARCHAR(255) NOT NULL,    -- "Ext.draw.Sprite"
-    member VARCHAR(255) NOT NULL, -- "method-setAttributes"
+    cls VARCHAR(100) NOT NULL,    -- "Ext.draw.Sprite"
+    member VARCHAR(100) NOT NULL, -- "method-setAttributes"
     -- the whole target must be unique
     UNIQUE KEY (domain, type, cls, member)
 );
