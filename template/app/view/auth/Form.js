a8184d2afdc9a2545ee89b2f3dbf7c6123a5b77b
Rene Saarsoo <nene@triin.net>
2012-10-05 13:40:06 -0700

    Extract a base class from Form and HeaderForm.
    Removing the duplication of login form HTML.

--- template/app/view/auth/Form.js
+++ template/app/view/auth/Form.js
@@ -2,61 +2,23 @@
  * View for login form at the end of comments.
  */
 Ext.define('Docs.view.auth.Form', {
-    extend: 'Ext.container.Container',
+    extend: 'Docs.view.auth.BaseForm',
     alias: 'widget.authForm',
-    requires: [
-        'Docs.Tip'
-    ],
-
-    html: [
-        '<span class="toggleNewComment"><span></span>Sign in to post a comment:</span>',
-        '<form class="loginForm">',
-            '<input class="username" type="text" name="username" placeholder="Username" />',
-            '<input class="password" type="password" name="password" placeholder="Password" />',
-            '<label><input type="checkbox" name="remember" /> Remember Me</label>',
-            '<input class="submit" type="submit" value="Sign in" />',
-            ' or ',
-            '<a class="register" href="http://www.sencha.com/forum/register.php" target="_blank">Register</a>',
-        '</form>'
-    ],
-
     cls: "new-comment",
 
-    /**
-     * @event login
-     * Fires when user fills in username and password and presses
-     * submit button.
-     * @param {Docs.view.auth.CommentForm} form
-     * @param {String} username
-     * @param {String} password
-     * @param {Boolean} remember True when remember-me checked.
-     */
+    initComponent: function() {
+        this.html = [
+            '<span class="toggleNewComment"><span></span>Sign in to post a comment:</span>',
+            this.createLoginFormHtml()
+        ];
+
+        this.callParent(arguments);
+    },
 
     afterRender: function() {
         this.callParent(arguments);
 
-        this.getEl().down("form").on("submit", this.submitLogin, this, {preventDefault: true});
-    },
-
-    // Gathers values from form and fires login event.
-    submitLogin: function(event, el) {
-        var form = Ext.get(el);
-        var username = form.down('input[name=username]').getValue();
-        var password = form.down('input[name=password]').getValue();
-        var rememberEl = form.down('input[name=remember]');
-
-        var remember = rememberEl ? !!(rememberEl.getAttribute('checked')) : false;
-
-        this.fireEvent("login", this, username, password, remember);
-    },
-
-    /**
-     * Shows the login failure message.
-     * @param {String} msg
-     */
-    showMessage: function(msg) {
-        var submitEl = this.getEl().down('input[type=submit]');
-        Docs.Tip.show(msg, submitEl, 'bottom');
+        this.bindFormSubmitEvent();
     }
 
 });
--- template/app/view/auth/HeaderForm.js
+++ template/app/view/auth/HeaderForm.js
@@ -2,33 +2,11 @@
  * View for login form in header.
  */
 Ext.define('Docs.view.auth.HeaderForm', {
-    extend: 'Ext.container.Container',
+    extend: 'Docs.view.auth.BaseForm',
     alias: 'widget.authHeaderForm',
     requires: [
-        'Docs.Tip',
         'Docs.Comments'
     ],
-
-    loginTplHtml: [
-        '<form class="loginForm">',
-            '<input class="username" type="text" name="username" placeholder="Username" />',
-            '<input class="password" type="password" name="password" placeholder="Password" />',
-            '<label><input type="checkbox" name="remember" /> Remember Me</label>',
-            '<input class="submit" type="submit" value="Sign in" />',
-            ' or ',
-            '<a class="register" href="http://www.sencha.com/forum/register.php" target="_blank">Register</a>',
-        '</form>'
-    ],
-
-    /**
-     * @event login
-     * Fires when user fills in username and password and presses
-     * submit button.
-     * @param {Docs.view.auth.HeaderForm} form
-     * @param {String} username
-     * @param {String} password
-     * @param {Boolean} remember True when remember-me checked.
-     */
 
     /**
      * @event logout
@@ -56,34 +34,8 @@ Ext.define('Docs.view.auth.HeaderForm', {
      * Shows login form.
      */
     showLoginForm: function() {
-        var tpl = Ext.create('Ext.Template', this.loginTplHtml.join(''));
-        tpl.overwrite(this.getEl(), {showLabel: this.showLabel});
-        this.bindSubmit();
-    },
-
-    bindSubmit: function() {
-        this.getEl().down("form").on("submit", this.submitLogin, this, {preventDefault: true});
-    },
-
-    // Gathers values from form and fires login event.
-    submitLogin: function(event, el) {
-        var form = Ext.get(el);
-        var username = form.down('input[name=username]').getValue();
-        var password = form.down('input[name=password]').getValue();
-        var rememberEl = form.down('input[name=remember]');
-
-        var remember = rememberEl ? !!(rememberEl.getAttribute('checked')) : false;
-
-        this.fireEvent("login", this, username, password, remember);
-    },
-
-    /**
-     * Shows the login failure message.
-     * @param {String} msg
-     */
-    showMessage: function(msg) {
-        var submitEl = this.getEl().down('input[type=submit]');
-        Docs.Tip.show(msg, submitEl, 'bottom');
+        this.update(this.createLoginFormHtml());
+        this.bindFormSubmitEvent();
     },
 
     /**
