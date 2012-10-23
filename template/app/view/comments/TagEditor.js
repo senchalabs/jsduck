2cc8d3e9a2a6bf4b8bfc4ea6080a449f718133a9
Rene Saarsoo <nene@triin.net>
2012-10-23 12:58:16 +0300

    Turn TagEditor into combo with some predefined tags.
    For now the list of tags is completely hardcoded.

--- template/app/view/comments/TagEditor.js
+++ template/app/view/comments/TagEditor.js
@@ -5,14 +5,31 @@ Ext.define("Docs.view.comments.TagEditor", {
     extend: "Ext.container.Container",
     floating: true,
     hidden: true,
-    style: "padding-top: 10px;",
+    componentCls: "comments-tageditor",
 
     initComponent: function() {
+        var tagnames = Ext.create('Ext.data.Store', {
+            fields: ['name'],
+            data: [
+                {name: "Fixed"},
+                {name: "Bug"},
+                {name: "Fixed in 4.1"}
+            ]
+        });
+
         this.items = [
             {
-                xtype: 'textfield',
+                xtype: 'combobox',
+                listConfig: {
+                    cls: "comments-tageditor-boundlist"
+                },
+                store: tagnames,
+                queryMode: "local",
+                displayField: "name",
+                valueField: "name",
                 enableKeyEvents: true,
                 listeners: {
+                    select: this.handleSelect,
                     blur: this.destroy,
                     keyup: this.onKeyUp,
                     scope: this
@@ -26,24 +43,28 @@ Ext.define("Docs.view.comments.TagEditor", {
     popup: function(el) {
         this.show();
         this.alignTo(el, 'bl', [-12, -2]);
-        this.down("textfield").focus(true, 100);
+        this.down("combobox").focus(true, 100);
     },
 
     onKeyUp: function(field, ev) {
         if (ev.keyCode === Ext.EventObject.ENTER) {
-            var value = Ext.String.trim(field.getValue());
-            if (value) {
-                /**
-                 * @event select
-                 * Fired when a tagname entered to the field and ENTER pressed.
-                 * @param {String} tagname
-                 */
-                this.fireEvent("select", value);
-            }
-            this.destroy();
+            this.handleSelect();
         }
         else if (ev.keyCode === Ext.EventObject.ESC) {
             this.destroy();
         }
+    },
+
+    handleSelect: function() {
+        var value = Ext.String.trim(this.down("combobox").getValue());
+        if (value) {
+            /**
+             * @event select
+             * Fired when a tagname entered to the field and ENTER pressed.
+             * @param {String} tagname
+             */
+            this.fireEvent("select", value);
+        }
+        this.destroy();
     }
 });
