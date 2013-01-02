956863612a0a937dcb4e59e049abdb7cbd7bb4d3
Rene Saarsoo <nene@triin.net>
2013-01-02 15:21:24 +0200

    Turn @removed into Tag class.
    Also extract base class for @removed and @deprecated.

--- lib/jsduck/builtins/deprecated.rb
+++ lib/jsduck/builtins/deprecated.rb
@@ -1,37 +1,10 @@
-require "jsduck/builtins/tag"
+require "jsduck/builtins/deprecated_tag"
 
 module JsDuck::Builtins
-  class Deprecated < Tag
+  class Deprecated < DeprecatedTag
     def initialize
-      @pattern = "deprecated"
       @key = :deprecated
-      @signature = {:long => "deprecated", :short => "DEP"}
-      @html_position = :bottom
-      @multiline = true
+      super
     end
-
-    def parse(p)
-      p.add_tag(:deprecated)
-      p.skip_horiz_white
-      p.current_tag[:version] = p.match(/[0-9.]+/) if p.look(/[0-9]/)
-    end
-
-    def process_doc(tags)
-      v = {:text => tags[0][:doc] || ""}
-      v[:version] = tags[0][:version] if tags[0][:version]
-      v
-    end
-
-    def to_html(context)
-      depr = context[:deprecated]
-      v = depr[:version] ? "since " + depr[:version] : ""
-      <<-EOHTML
-        <div class='signature-box deprecated'>
-        <p>This #{context[:tagname]} has been <strong>deprecated</strong> #{v}</p>
-        #{format(depr[:text])}
-        </div>
-      EOHTML
-    end
-
   end
 end
