1395a203ef398424a8557177aecb7a072b1d0fa2
Rene Saarsoo <nene@triin.net>
2013-01-08 13:09:29 +0200

    Improve wording of 404 page.
    Don't direct people right away to JSDuck issue tracker - most likely
    it's just a fault of a broken link and it's the authors of the
    documentation who should fix it.

--- template/app/controller/Failure.js
+++ template/app/controller/Failure.js
@@ -25,9 +25,12 @@ Ext.define('Docs.controller.Failure', {
         var tpl = new Ext.XTemplate(
             "<h1>Oops...</h1>",
             "<p>{msg}</p>",
-            "<p>Maybe it was renamed to something else? Or maybe your internet connection has failed? ",
-            "This would be sad. Hopefully it's just a bug on our side. ",
-            "Report it to <a href='https://github.com/senchalabs/jsduck/issues'>JSDuck issue tracker</a> if you feel so.</p>",
+            "<p>Maybe it was renamed to something else?<br> Or maybe your internet connection has failed?<br> ",
+            "This would be sad. Hopefully it's just a bug on our side.</p>",
+            "<p>Most likely you just followed a broken link inside this very documentation. ",
+            "Go and report it to the authors of the docs.</p>",
+            "<p>But if you think it's a bug in JSDuck documentation-generator itself, feel free to open ",
+            "an issue at the <a href='https://github.com/senchalabs/jsduck/issues'>JSDuck issue tracker</a>.</p>",
             "<p>Sorry for all this :(</p>"
         );
         Ext.getCmp("failure").update(tpl.apply({msg: msg}));
