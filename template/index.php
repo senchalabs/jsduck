9f21b9d47928ac9e7c17a80791fc131ef5e35c03
Ben Hatfield <bhatfield@appcelerator.com>
2013-01-14 10:13:08 -0800

    [TIDOC-931] Generate only if _escaped_fragment_ is set.

--- template/index.php
+++ template/index.php
@@ -34,7 +34,7 @@ function decode_file($filename) {
 if (isset($_GET["_escaped_fragment_"]) || isset($_GET["print"])) {
   $fragment = isset($_GET["_escaped_fragment_"]) ? $_GET["_escaped_fragment_"] : $_GET["print"];
   try {
-    if (preg_match('/^\/api\/([^-]+)-([^-]+)-(.+)/', $fragment, $m)) {
+    if (isset($_GET["_escaped_fragment_"]) && preg_match('/^\/api\/([^-]+)-([^-]+)-(.+)/', $fragment, $m)) {
       $className = $m[1];
       $attrType = $m[2];
       $attrName = $m[3];
