3bde89f9538bae76e4fb46c2e74a3431cadd1f86
Rene Saarsoo <nene@triin.net>
2013-01-23 19:00:37 +0200

    Extract Process::GlobalMembers from BatchParser.
    Moving all the filter_class logic to GlobalMembers processor,
    which can take care of the global member corner case without
    disturbing the main BatchParser logic.

--- lib/jsduck/batch_parser.rb
+++ lib/jsduck/batch_parser.rb
@@ -6,6 +6,7 @@ require 'jsduck/class'
 require 'jsduck/relations'
 require 'jsduck/logger'
 require 'jsduck/process/ignored_classes'
+require 'jsduck/process/global_members'
 require 'jsduck/process/enums'
 require 'jsduck/process/accessors'
 require 'jsduck/process/ext4_events'
@@ -33,7 +34,7 @@ module JsDuck
     def run
       @parsed_files = parallel_parse(@opts.input_files)
       result = aggregate(@parsed_files)
-      @relations = filter_classes(result)
+      @relations = to_class_objects(result)
       apply_extra_processing
       return @relations
     end
@@ -63,6 +64,7 @@ module JsDuck
       classes_hash = agr.result
 
       Process::IgnoredClasses.new(classes_hash).process_all!
+      Process::GlobalMembers.new(classes_hash, @opts).process_all!
       Process::Accessors.new(classes_hash).process_all!
       Process::Ext4Events.new(classes_hash, @opts).process_all!
       Process::Enums.new(classes_hash).process_all!
@@ -72,29 +74,10 @@ module JsDuck
       classes_hash.values
     end
 
-    # Turns all aggregated data into Class objects.
-    # Depending on --ignore-global either keeps or discards the global class.
-    # Warnings for global members are printed regardless of that setting,
-    # but of course can be turned off using --warnings=-global
-    def filter_classes(docs)
-      classes = []
-      docs.each do |d|
-        cls = Class.new(d)
-        if d[:name] != "global"
-          classes << cls
-        else
-          # add global class only if --ignore-global not specified
-          classes << cls unless @opts.ignore_global
-
-          # Print warning for each global member
-          cls.all_local_members.each do |m|
-            type = m[:tagname].to_s
-            name = m[:name]
-            file = m[:files][0]
-            Logger.warn(:global, "Global #{type}: #{name}", file[:filename], file[:linenr])
-          end
-        end
-      end
+    # Turns all aggregated data into Class objects and places the
+    # classes inside Relations container.
+    def to_class_objects(docs)
+      classes = docs.map {|d| Class.new(d) }
       Relations.new(classes, @opts.external_classes)
     end
 
