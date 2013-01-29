83c8293fedc6e9f33e6fcb627648e652f934c615
Rene Saarsoo <nene@triin.net>
2013-01-29 18:40:38 +0200

    Restore HTML rendering of subproperties.
    The rendering of @cfg, @property, @css_var subproperties got lost during
    the previous refactorings.
    
    Add a special Subproperties tag class to render the :properties field.

---
+++ lib/jsduck/tag/subproperties.rb
@@ -0,0 +1,21 @@
+require "jsduck/tag/tag"
+require "jsduck/subproperties"
+
+module JsDuck::Tag
+  # There is no @subproperties tag.
+  #
+  # This tag class exists solely to implement rendering of :properties
+  # field which can exist in :cfg, :property or :css_var members.
+  class Subproperties < Tag
+    def initialize
+      @key = :properties
+      # Reuse the PARAMS position - a member either has properties or
+      # params, not both.
+      @html_position = POS_PARAMS
+    end
+
+    def to_html(m)
+      JsDuck::Subproperties.render(m)
+    end
+  end
+end
