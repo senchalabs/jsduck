df2a465b88ecacf0de735a377b07dab9d402ffaa
Rene Saarsoo <nene@triin.net>
2013-02-27 15:18:02 +0200

    Parse and process @author and @docauthor tags.
    But still create no output.  Simply make it easier for somebody
    to override the tag and add the output capability.

--- lib/jsduck/tag/docauthor.rb
+++ lib/jsduck/tag/docauthor.rb
@@ -4,7 +4,9 @@ module JsDuck::Tag
   # Exactly the same as @author tag - it's simply ignored.
   class Docauthor < Author
     def initialize
+      super
       @pattern = "docauthor"
+      @tagname = :docauthor
     end
   end
 end
