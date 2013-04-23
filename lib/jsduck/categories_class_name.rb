a75348207f5e1196488ad9d863e8c42daec84383
Rene Saarsoo <nene@triin.net>
2013-04-23 17:24:42 +0300

    Remove new-methods icons from API overview page.
    Leave just small stars behind new classes.  This way it's lot
    less cluttered.
    
    The new "New in this version" section covers the details of
    what's new much better and doesn't get on the way.

--- lib/jsduck/categories_class_name.rb
+++ lib/jsduck/categories_class_name.rb
@@ -26,34 +26,10 @@ module JsDuck
     # Adds small star to new classes in the current version.
     def render_new_label(cls)
       if cls[:meta][:new]
-        "&nbsp;<span class='new-class' title='New class'>#{stars(1)}</span>"
+        "&nbsp;<span class='new-class' title='New class'>&#9733;</span>"
       else
-        n = new_members_count(cls)
-        if n > 0
-          title = "#{n} new member#{(n>1) ? 's' : ''}"
-          "&nbsp;<span class='new-members' title='#{title}'>#{stars(n)}</span>"
-        else
-          ""
-        end
+        ""
       end
-    end
-
-    # Produces string of n stars.
-    # First 3 stars are rendered as "<unicode-star>", the following as "+".
-    # At max 15 stars are rendered.
-    def stars(n)
-      if n > 15
-        stars(3) + ("+" * (15-3))
-      elsif n > 3
-        stars(3) + ("+" * (n-3))
-      else
-        "&#9733;" * n
-      end
-    end
-
-    # Returns number of new members the class has in the current version
-    def new_members_count(cls)
-      cls.find_members(:local => true).find_all {|m| m[:meta][:new] && !m[:private] }.length
     end
 
   end
