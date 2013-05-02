85cc97956546877b304850c7b4d639db8cff79ac
Rene Saarsoo <nene@triin.net>
2013-05-02 17:47:53 +0300

    Move node body extraction to Node class.

--- lib/jsduck/js/fires.rb
+++ lib/jsduck/js/fires.rb
@@ -1,5 +1,4 @@
 require "jsduck/util/singleton"
-require "jsduck/js/node_array"
 
 module JsDuck
   module Js
@@ -31,7 +30,7 @@ module JsDuck
             var_name = node["id"].to_s
             @this_map[var_name] = true
           else
-            events.concat(detect_body(extract_body(node)))
+            events.concat(detect_body(node.body))
           end
         end
 
@@ -52,30 +51,6 @@ module JsDuck
       # True when initialization of variable with `this`
       def this_var?(node)
         node.type == "VariableDeclarator" && node["init"].type == "ThisExpression"
-      end
-
-      # Extracts all sub-statements and sub-expressions from AST node.
-      # Without looking at the type of node, we just take all the
-      # sub-hashes and -arrays.
-      #
-      # A downside of this simple algorithm is that the statements can
-      # end up in different order than they are in source code.  For
-      # example the IfStatement has three parts in the following
-      # order: "test", "consequent", "alternate": But because we're
-      # looping over a hash, they might end up in a totally different
-      # order.
-      def extract_body(node)
-        body = []
-        node.raw.each_pair do |key, value|
-          if key == "type" || key == "range"
-            # ignore
-          elsif value.is_a?(Array)
-            node[key].each {|n| body << n }
-          elsif value.is_a?(Hash)
-            body << node[key]
-          end
-        end
-        body
       end
 
     end
