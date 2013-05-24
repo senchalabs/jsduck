c816fec65f725381cc8cbcbb64a5a1bfd7280019
Rene Saarsoo <nene@triin.net>
2013-05-24 15:24:33 +0300

    Up version to 4.10.3.

--- lib/jsduck/options.rb
+++ lib/jsduck/options.rb
@@ -94,7 +94,7 @@ module JsDuck
       @ext4_events = nil
       @meta_tag_paths = []
 
-      @version = "4.10.2"
+      @version = "4.10.3"
 
       # Customizing output
       @title = "Documentation - JSDuck"
