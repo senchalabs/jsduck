40cd5d82e3d4f7bfdc527f9782d0bbf6296b7ba7
Rene Saarsoo <nene@triin.net>
2013-06-04 15:12:46 +0300

    Fix benchmark scripts.

--- spec/benchmark/parse.rb
+++ spec/benchmark/parse.rb
@@ -2,10 +2,9 @@
 #
 # Takes bunch of filenames as arguments and runs them all through JsParser.
 #
-$:.unshift File.dirname(File.dirname(__FILE__)) + "/lib"
+$:.unshift File.dirname(File.dirname(File.dirname(__FILE__))) + "/lib"
 require 'jsduck/js_parser'
 
 ARGV.each do |fname|
   JsDuck::JsParser.new(IO.read(fname)).parse
 end
-
--- spec/benchmark/serialize.rb
+++ spec/benchmark/serialize.rb
@@ -3,7 +3,7 @@
 # Takes bunch of filenames as arguments, runs them all through esprima
 # parser and serializes the resulting syntax trees.
 #
-$:.unshift File.dirname(File.dirname(__FILE__)) + "/lib"
+$:.unshift File.dirname(File.dirname(File.dirname(__FILE__))) + "/lib"
 require 'jsduck/esprima'
 require 'jsduck/serializer'
 
@@ -11,4 +11,3 @@ ARGV.each do |fname|
   ast = JsDuck::Esprima.instance.parse(IO.read(fname))
   JsDuck::Serializer.new.to_s(ast)
 end
-
