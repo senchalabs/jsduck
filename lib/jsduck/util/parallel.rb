658c43ab9865e88ee26d8078a51405103d2367f6
Rene Saarsoo <nene@triin.net>
2013-10-02 16:58:31 +0300

    Require rubygems inside the smoke test instead.
    Drop the statement from JSDuck code itself.

--- lib/jsduck/util/parallel.rb
+++ lib/jsduck/util/parallel.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'parallel'
 
 module JsDuck
--- spec/smoke_spec.rb
+++ spec/smoke_spec.rb
@@ -2,7 +2,9 @@
 describe "smoke test running jsduck --version" do
 
   it "does not crash" do
-    `./bin/jsduck --version`
+    # Explicitly include rubygems, otherwise TravisCI will fail to
+    # load the parallel and possibly other gems in Ruby 1.8
+    `ruby -e 'require "rubygems"' ./bin/jsduck --version`
     $?.exitstatus.should == 0
   end
 
