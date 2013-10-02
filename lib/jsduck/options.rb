a3b57e19a8d92334f2ca6b6d0317c867682be694
Rene Saarsoo <nene@triin.net>
2013-10-02 16:28:59 +0300

    Up version to 5.3.1.

--- lib/jsduck/options.rb
+++ lib/jsduck/options.rb
@@ -102,7 +102,7 @@ module JsDuck
       ]
       @ext4_events = nil
 
-      @version = "5.3.0"
+      @version = "5.3.1"
       # Customizing output
       @title = "Documentation - JSDuck"
       @header = "<strong>Documentation</strong> JSDuck"
