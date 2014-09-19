10e16a229b78d96cc90944932e29a317e160a706
Tim Statler <tstatler@appcelerator.com>
2014-09-18 23:22:52 -0400

    [TIDOC-1847] Added multi-level TOC to cloud guide pages.
    Number of <hN> tags that are included in TOC is determined
    by `max_level` property.

--- lib/jsduck/guide_toc.rb
+++ lib/jsduck/guide_toc.rb
@@ -10,14 +10,24 @@ module JsDuck
     def self.inject(html, guide_name)
       toc = []
       new_html = []
+      max_level = 4
+
+      # Count the number of heading increments we've seen so far; use one fewer
+      # than max_level, since <h1> tags don't go in the TOC.
+      heading_counts = Array.new(max_level - 1) { 0 }      
 
       html.each_line do |line|
-        if line =~ /^\s*<(h[1-6])>(.*?)<\/h[1-6]>$/
+        if line =~ /^\s*<(h([1-6]))>(.*?)<\/h[1-6]>$/
+
           tag = $1
-          text = Util::HTML.strip_tags($2)
+           level = $2.to_i - 1 # ignore <h1>
+           text = Util::HTML.strip_tags($3)
           id = guide_name + "-section-" + title_to_id(text)
-          if tag == "h2"
-            toc << "<li><a href='#!/guide/#{id}'>#{text}</a></li>\n"
+          if (1...max_level).include? level
+             heading_counts[level - 1] += 1
+             (level...heading_counts.length).each { |i| heading_counts[i] = 0 }
+             prefix = heading_counts.slice(0...level).join('.')
+             toc << "#{prefix}. <a href='#!/guide/#{id}'>#{text}</a><br/>\n"
           end
           new_html << "<#{tag} id='#{id}'>#{text}</#{tag}>\n"
         else
